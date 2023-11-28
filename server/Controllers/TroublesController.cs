ebbd764fdac3ea981fc43cb0444e995c7e88a9b3
Арслан Курбанов <arslik35@gmail.com>
2023-11-28 18:53:22 +0300

    createDb

---
+++ server/Controllers/TroublesController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using swetHome.Context;
+
+namespace swetHome.Controllers
+{
+	public class TroublesController : Controller
+	{
+		public static readonly HttpClient client = new HttpClient();
+		public readonly TroublesContextDb _troublescontextDb;
+
+		public TroublesController(TroublesContextDb troublescontextDatabase)
+		{
+			_troublescontextDb = troublescontextDatabase;
+
+		}
+		[HttpGet]
+		[Route("Troubles")]
+		public async Task<IActionResult> AllTroubles(int _limitTr, int pageTr = 1)
+		{
+
+
+
+
+			if (_limitTr <= 0 || pageTr <= 0)
+			{
+
+				return Ok(await _troublescontextDb.Troubles!.ToListAsync());
+			}
+			var result1 = await _troublescontextDb.Troubles!
+				  .OrderBy(x => x.TroublesId)
+				  .Take(pageTr * _limitTr)
+					 .ToListAsync();
+
+
+
+			return Ok(result1);
+
+
+
+
+
+		}
+	}
+}
---
+++ server/Models/Users.cs
@@ -0,0 +1,25 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Xml.Serialization;
+
+namespace swetHome.Models
+{
+	[Table("Users",Schema ="SweetHome")]
+	public class Users
+	{
+		[Key]
+		public long UsersId { get; set; }
+		public string Email { get; set; }
+		public string Password { get; set; }
+		public double PhoneNumber { get; set; }
+		public string Adress { get; set; }
+		public string Name { get; set; }
+		public string SurName { get; set; }
+		public string LastName { get; set; }
+
+
+
+		
+	}
+}
